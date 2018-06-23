43d45b8c6365f9920f74f91788c32b5dc8019cb7
Ujjwal Jain <ujjwal451@gmail.com>
2018-06-23 19:44:37 +0530

    Add files via upload

---
+++ public/scripts/admin.js
@@ -0,0 +1,28 @@
+$(function(){
+
+    $('#submititem').click(function(){
+
+
+        alert("Item added")
+        
+         var item=($('#title')[0]).value;
+         var description=($('#description')[0]).value;
+        var image=($('#imglink')[0]).value;
+        var price=($('#price')[0]).value;
+        var element={item:item,
+                     description:description,
+                     image:image,
+                     price:price
+                    }
+
+        $.post(
+            '/additem',
+            {
+                'json':JSON.stringify(element)
+            },
+            function(){
+
+            }
+        );
+    });
+});
